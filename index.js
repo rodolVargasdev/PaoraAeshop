92e0037b6f34a581a083c2e6668fb7f55f2157d1
hawokdev <70559204+hawokdev@users.noreply.github.com>
2022-06-30 15:57:12 -0600

    Terminada la primera versión del home de la Clothe Shop

--- index.js
+++ index.js
@@ -1,43 +1,96 @@
 const botonR = document.querySelector(".main__shop-button-right");
+const botonL = document.querySelector(".main__shop-button-left");
 var contador = 0;
 
-if(contador = 0)
-{
     document.querySelectorAll("#shop__item")[0].classList.add('show-once');
-    contador = 1;
-}
+ 
+
+document.querySelectorAll("#shop__item")[0].classList.add('shop__item-select');
+document.querySelectorAll("#shop__item")[0].classList.remove('shop__item');
+
+document.querySelectorAll("#shop__item-hide")[0].classList.add('shop__');
+document.querySelectorAll("#shop__item-hide")[0].classList.remove('shop__item-hide');
+
+var i = 0;
+var j = 0;
 
 function goliat ()
 {
-    let i = 1;
-    document.querySelectorAll("#shop__item")[1].classList.add('shop__item-select');
-    document.querySelectorAll("#shop__item")[1].classList.remove('shop__item');
-    i--;
-    document.querySelectorAll("#shop__item")[0].classList.remove('shop__item-select');
-    document.querySelectorAll("#shop__item")[0].classList.add('shop__item');
 
-    document.querySelectorAll("#shop__item-hide")[1].classList.add('shop__');
-    document.querySelectorAll("#shop__item-hide")[1].classList.remove('shop__item-hide');
-    document.querySelectorAll("#shop__item-hide")[0].classList.remove('shop__');
-    document.querySelectorAll("#shop__item-hide")[0].classList.add('shop__item-hide');
+    if(i==2)
+    {
+        document.querySelectorAll(".main__shop-container")[0].classList.add('animation-right');
+        document.querySelectorAll(".main__shop-container")[0].classList.remove('animation-left');
+    }
+
+    i++;
+
+    if(i>4)
+    {
+        i = 4;
+    }
+
+    document.querySelectorAll("#shop__item")[i].classList.add('shop__item-select');
+    document.querySelectorAll("#shop__item")[i].classList.remove('shop__item');
     
-}
-botonR.addEventListener("click", goliat);
+    document.querySelectorAll("#shop__item")[i-1].classList.remove('shop__item-select');
+    document.querySelectorAll("#shop__item")[i-1].classList.add('shop__item');
 
-const botonL = document.querySelector(".main__shop-button-left");
+    document.querySelectorAll("#shop__item-hide")[i].classList.add('shop__');
+    document.querySelectorAll("#shop__item-hide")[i].classList.remove('shop__item-hide');
+    
+    document.querySelectorAll("#shop__item-hide")[i-1].classList.remove('shop__');
+    document.querySelectorAll("#shop__item-hide")[i-1].classList.add('shop__item-hide');
+    
+    console.log(i);
+    if(i==4)
+    {
+        document.querySelectorAll(".main__shop-button-right")[0].classList.add('button-rigth-hide');
+    }
+    else if(i!=4)
+    {
+        document.querySelectorAll(".main__shop-button-right")[0].classList.remove('button-rigth-hide');
+    }
+}   
+
+
 function goliat2 ()
 {   
-    let i = 1;
-    document.querySelectorAll("#shop__item")[0].classList.add('shop__item-select');
-    document.querySelectorAll("#shop__item")[0].classList.remove('shop__item');
-    i--;
-    document.querySelectorAll("#shop__item")[1].classList.remove('shop__item-select');
-    document.querySelectorAll("#shop__item")[1].classList.add('shop__item');
+    
+    if(i==2)
+    {
+        document.querySelectorAll(".main__shop-container")[0].classList.remove('animation-right');
+        document.querySelectorAll(".main__shop-container")[0].classList.add('animation-left');
+    }
+    if(i<=0)
+    {
+        i = 0;
+    }
+    
 
-    document.querySelectorAll("#shop__item-hide")[0].classList.add('shop__');
-    document.querySelectorAll("#shop__item-hide")[0].classList.remove('shop__item-hide');
-    document.querySelectorAll("#shop__item-hide")[1].classList.remove('shop__');
-    document.querySelectorAll("#shop__item-hide")[1].classList.add('shop__item-hide');
+    document.querySelectorAll("#shop__item")[i-1].classList.add('shop__item-select');
+    document.querySelectorAll("#shop__item")[i-1].classList.remove('shop__item');
+
+    document.querySelectorAll("#shop__item")[i].classList.remove('shop__item-select');
+    document.querySelectorAll("#shop__item")[i].classList.add('shop__item');
+
+    document.querySelectorAll("#shop__item-hide")[i-1].classList.add('shop__');
+    document.querySelectorAll("#shop__item-hide")[i-1].classList.remove('shop__item-hide');
+    
+    document.querySelectorAll("#shop__item-hide")[i].classList.remove('shop__');
+    document.querySelectorAll("#shop__item-hide")[i].classList.add('shop__item-hide');
+
+    i--;
+    if(i==4)
+    {
+        document.querySelectorAll(".main__shop-button-right")[0].classList.add('button-rigth-hide');
+    }
+    else if(i!=4)
+    {
+        document.querySelectorAll(".main__shop-button-right")[0].classList.remove('button-rigth-hide');
+    }
+    console.log(i);
 
 }
 botonL.addEventListener("click", goliat2);
+botonR.addEventListener("click", goliat);
